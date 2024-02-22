3e43f6805b507372c8f0c25fd7f1b3c6f92b0c9d
Pavan Kumar <pavankittu19@gmail.com>
2024-02-22 13:28:24 +0530

    added router/react to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import router from "./routes";
+import { RouterProvider } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <RouterProvider router={router}/>
     </div>
   )
 }
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="text-green-400">
+            <h1>This is my landing Page</h1>
+        </div>
+    )
+ }
+ export default HomePage
